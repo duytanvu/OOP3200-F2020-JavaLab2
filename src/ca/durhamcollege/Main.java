1ad00f002957c4afcabf9bd1b9b3c559ef88e6d3
Domenic Catalano <domenic.catalano@dcmail.ca>
2020-11-19 08:44:59 -0500

    Initial Commit

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -1,3 +1,9 @@
+/*
+    Author: Domenic Catalano & Duy Tan Vu
+    Date: Sunday, November 19, 2020
+    Program Name: OOP 3200 Java Lab 2
+ */
+
 package ca.durhamcollege;
 
 public class Main {
---
+++ src/ca/durhamcollege/WorkTicket.java
@@ -0,0 +1,31 @@
+/*
+    Author: Domenic Catalano & Duy Tan Vu
+    Date: Sunday, November 19, 2020
+    Program Name: OOP 3200 Java Lab 2
+ */
+
+package ca.durhamcollege;
+
+public class WorkTicket
+{
+    // PRIVATE INSTANCE VARIABLES
+    private int myTicketNumber;
+    private String myClientID;
+    private MyDate myDate;
+    private String myDescription;
+
+    // PUBLIC PROPERTIES
+
+
+    // CONSTRUCTORS
+
+
+    // PRIVATE METHODS
+
+
+    // PUBLIC METHODS
+
+
+    // STATIC METHODS
+
+}
