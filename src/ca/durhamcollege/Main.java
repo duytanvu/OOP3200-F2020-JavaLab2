9e7dac285ab6841ed7f8b5dbabfe3b77dbf6f1e9
Tan Vu <vuduytann@gmail.com>
2020-11-20 21:25:00 -0500

    Convert C++ code for WorkTicket to Java code

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -6,9 +6,25 @@
 
 package ca.durhamcollege;
 
-public class Main {
+import java.time.LocalDate;
 
+public class Main {
     public static void main(String[] args) {
-	// write your code here
+        try {
+
+            WorkTicket firstWorkTicket = new WorkTicket();
+            WorkTicket secondWorkTicket = new WorkTicket(2, "abc", LocalDate.of(2000, 12, 12), "something");
+            System.out.println(firstWorkTicket.toString());
+            System.out.println(secondWorkTicket.toString());
+
+            if (firstWorkTicket.setWorkTicket(1, "abc", LocalDate.of(2020, 10, 10), "dhasufh"))
+                System.out.println("\nErrors! No changes to the ticket made.");
+
+            if (secondWorkTicket.setWorkTicket(1, "abc", LocalDate.of(2020, 10, 10), "dhasufh"))
+                System.out.println("\nErrors! No changes to the ticket made.");
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println(illegalArgumentException);
+        }
+
     }
 }
--- src/ca/durhamcollege/WorkTicket.java
+++ src/ca/durhamcollege/WorkTicket.java
@@ -6,28 +6,118 @@
 
 package ca.durhamcollege;
 
-public class WorkTicket
-{
+import java.time.LocalDate;
+
+public class WorkTicket {
     // PRIVATE INSTANCE VARIABLES
+    final int MINIMUM_YEAR = 2000;
+    final int MAXIMUM_YEAR = 2099;
     private int myTicketNumber;
     private String myClientID;
-    private String day;
-    private String month;
-    private String year;
+    private java.time.LocalDate myDate;
     private String myDescription;
 
     // PUBLIC PROPERTIES
+    public int getMyTicketNumber() {
+        return myTicketNumber;
+    }
 
+    public void setMyTicketNumber(int myTicketNumber) {
+        if (myTicketNumber <= 0) {
+            throw new IllegalArgumentException("Work ticket number must be greater than 0.");
+        } else {
+            this.myTicketNumber = myTicketNumber;
+        }
+    }
+
+    public String getMyClientID() {
+        return myClientID;
+    }
+
+    public void setMyClientID(String myClientID) {
+        if (myClientID.length() < 1) {
+            throw new IllegalArgumentException("Client ID and issue description must be at least 1 in length");
+        } else {
+            this.myClientID = myClientID;
+        }
+    }
+
+    public LocalDate getMyDate() {
+        return myDate;
+    }
+
+    public void setMyDate(LocalDate myDate) {
+        int year = myDate.getYear();
+        if (year < MINIMUM_YEAR || year > MAXIMUM_YEAR) {
+            throw new IllegalArgumentException("Year must be in the range 2000-2099 inclusive");
+        } else {
+            this.myDate = myDate;
+        }
+    }
+
+    public String getMyDescription() {
+        return myDescription;
+    }
+
+    public void setMyDescription(String myDescription) {
+        if (myDescription.length() < 1) {
+            throw new IllegalArgumentException("Client ID and issue description must be at least 1 in length");
+        } else {
+            this.myDescription = myDescription;
+        }
+    }
 
     // CONSTRUCTORS
+    WorkTicket() {
+        myTicketNumber = 0;
+        myClientID = null;
+        myDate = null;
+        myDescription = null;
+    }
 
-
-    // PRIVATE METHODS
+    WorkTicket(int myTicketNumber, String myClientID, LocalDate myDate, String myDescription) {
+        int year = myDate.getYear();
+        if (year < MINIMUM_YEAR || year > MAXIMUM_YEAR) {
+            throw new IllegalArgumentException("Year must be in the range 2000-2099 inclusive");
+        } else if (myTicketNumber <= 0) {
+            throw new IllegalArgumentException("Work ticket number must be greater than 0.");
+        } else if (myClientID.length() < 1 || myDescription.length() < 1) {
+            throw new IllegalArgumentException("Client ID and issue description must be at least 1 in length");
+        } else {
+            setMyTicketNumber(myTicketNumber);
+            setMyClientID(myClientID);
+            setMyDate(myDate);
+            setMyDescription(myDescription);
+        }
+    }
 
 
     // PUBLIC METHODS
+    public boolean setWorkTicket(int myTicketNumber, String myClientID, LocalDate myDate, String myDescription) {
+        boolean isValid = true;
 
 
-    // STATIC METHODS
+        if (myTicketNumber <= 0 || myDate.getYear() < MINIMUM_YEAR || myDate.getYear() > MAXIMUM_YEAR) {
+            isValid = false;
+        } else if (myClientID.length() < 1 || myDescription.length() < 1) {
+            isValid = false;
+        }
 
+        if (isValid) {
+            setMyTicketNumber(myTicketNumber);
+            setMyClientID(myClientID);
+            setMyDate(myDate);
+            setMyDescription(myDescription);
+        }
+
+        return isValid;
+    }
+
+    @Override
+    public String toString() {
+        return "\nWork Ticket #: " + myTicketNumber +
+                "\nClient ID    : " + myClientID +
+                "\nDate         : " + myDate +
+                "\nIssue        : " + myDescription;
+    }
 }
